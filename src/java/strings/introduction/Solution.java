3ef811c352efde043471024b1b90479cc07829bb
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-02-26 19:18:40 -0600

    HackerRank exercise setup.

---
+++ src/java/strings/introduction/Solution.java
@@ -0,0 +1,21 @@
+package java.strings.introduction;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        //--| Sum the lengths of A and B.
+
+        //--| Determine if A is lexicographically larger than B (i.e.: does B come before A in the dictionary?).
+
+        //--| Capitalize the first letter in A and B and print them on a single line, separated by a space.
+
+    }
+}
