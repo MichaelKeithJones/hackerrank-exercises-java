1f386be77f8989ec12b2cb0b77d71f9d58e34e8e
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-02-26 19:34:29 -0600

    Completed HackerRank exercise.

---
+++ src/strings/introduction/Solution.java
@@ -0,0 +1,29 @@
+package strings.introduction;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        //--| Sum the lengths of A and B.
+            int sumOfLengths = A.length() + B.length();
+            System.out.println(sumOfLengths);
+        //--| Determine if A is lexicographically larger than B (i.e.: does B come before A in the dictionary?).
+            if (B.compareTo(A) < 0) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        //--| Capitalize the first letter in A and B and print them on a single line, separated by a space.
+            String aCapitalize = A.substring(0, 1).toUpperCase() + A.substring(1);
+            String bCapitalize = B.substring(0, 1).toUpperCase() + B.substring(1);
+            String combinedString = aCapitalize + " " + bCapitalize;
+            System.out.println(combinedString);
+    }
+}
