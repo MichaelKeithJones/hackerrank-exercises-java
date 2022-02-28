df6b548f7bf3480c4679a42d938c1a8ee0934b84
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-02-28 10:03:31 -0600

    HackerRank Exercise completed.

--- src/currency/formatter/Solution.java
+++ src/currency/formatter/Solution.java
@@ -21,11 +21,37 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         double payment = sc.nextDouble();
 
-
         System.out.printf("US: %s%n", us.format(payment));
         System.out.printf("India: Rs.%s%n", india.format(payment));
         System.out.printf("China: %s%n", china.format(payment));
         System.out.printf("France: %s%n", france.format(payment));
 
+        //--| Solution on HackerRank
+        //--| Note: Had to run the below Java 8 version to get it to work on HackerRank
+
+        //    import java.io.*;
+        //    import java.util.*;
+        //    import java.text.*;
+        //    import java.math.*;
+        //    import java.util.regex.*;
+        //
+        //    public class Solution {
+        //
+        //        public static void main(String[] args) {
+        //            Scanner scanner = new Scanner(System.in);
+        //            double payment = scanner.nextDouble();
+        //            scanner.close();
+        //
+        //            String us = NumberFormat.getCurrencyInstance(new Locale("en", "US")).format(payment);
+        //            String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        //            String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        //            String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        //
+        //            System.out.println("US: " + us);
+        //            System.out.println("India: " + india);
+        //            System.out.println("China: " + china);
+        //            System.out.println("France: " + france);
+        //        }
+        //    }
     }
 }
