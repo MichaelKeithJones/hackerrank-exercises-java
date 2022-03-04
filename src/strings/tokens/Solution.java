b6c85335bb2e77d83dd9a05d1348cbbd23c09ab6
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-03-04 14:46:41 -0600

    HackerRank Exercise Complete.

---
+++ src/strings/tokens/Solution.java
@@ -0,0 +1,31 @@
+package strings.tokens;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        input = input.replace("!", " ");
+        input = input.replace(",", " ");
+        input = input.replace("?", " ");
+        input = input.replace(".", " ");
+        input = input.replace("_", " ");
+        input = input.replace("'", " ");
+        input = input.replace("@", " ");
+
+        String [] output = input.split("\\s+");
+
+        ArrayList<String> aList = new ArrayList<String>(Arrays.asList(output));
+        aList.removeIf(b -> (b.length() == 0));
+        System.out.println(aList.size());
+
+        for(String token: aList) {
+            System.out.println(token);
+        }
+    }
+}
