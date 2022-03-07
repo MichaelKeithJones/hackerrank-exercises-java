c191b1ff0ef47d60390d4340f2c675d7ca684786
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-03-07 10:44:52 -0600

    Completed HackRank Exercise 'Regex Practice'.

---
+++ src/regex/Solution.java
@@ -0,0 +1,23 @@
+package regex;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main(String [] args) {
+
+        Scanner get = new Scanner(System.in);
+        String field = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
+        String regex = "(" + field + "\\." + field + "\\." + field + "\\." + field + ")";
+        HashMap<String, Boolean> map = new HashMap<>();
+
+        for(int i = 0; i < 6; i++) {
+            String input = get.next();
+            map.put(input, Pattern.compile(regex).matcher(input).matches());
+        }
+        for(String key: map.keySet()) { System.out.println(map.get(key)); }
+    }
+}
