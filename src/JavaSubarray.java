b01873663f390844c7d255eb57f3bf6f7b729e1c
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-03-25 13:52:51 -0500

    Completed HackerRank exercise'Java Aubarray'.

---
+++ src/JavaSubarray.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaSubarray {
+
+    public static void main(String[] args) {
+
+        Scanner get = new Scanner(System.in);
+        final int n = Integer.parseInt(get.nextLine());
+        int [] A = new int[n];
+        for (int i = 0; i < n; i++) { A[i] = get.nextInt(); }
+
+        int count = 0;
+        for(int k = 0; k < n; k++) {
+            for (int j = n; j > k; j--) {
+                int sum = 0;
+                for (int i = k; i < j; i++) {
+                    sum += A[i];
+                }
+                if (sum < 0) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
