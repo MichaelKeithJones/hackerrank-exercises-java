8cd6622ec7d6c679d84c95275a4c0ac9eeafeb05
MichaelKeithJones <michael.k.jones.20@gmail.com>
2022-04-04 18:36:15 -0500

    Completed HackerRack exercise 'JavaMap'.

---
+++ src/JavaMap.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class JavaMap {
+
+    public static void main(String[] args) {
+        try {
+            Map<String, Integer> map = new HashMap<>();
+
+            BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(b.readLine());             // Number of Entries
+
+            for (int i = 0; i < n; i++) {
+                String name = b.readLine();                     // Name
+                int phone = Integer.parseInt(b.readLine());     // Phone Number
+                map.put(name, phone);
+            }
+
+            String query;
+            while (!(query = b.readLine().trim()).isEmpty()) {
+                if (map.containsKey(query)) {
+                    System.out.println(query + "=" + map.get(query));
+                } else {
+                    System.out.println("Not found");
+                }
+            }
+
+            b.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
