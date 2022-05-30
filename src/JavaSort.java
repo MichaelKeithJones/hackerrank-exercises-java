96c56c7ddabf92e7aa9f430ec35e0fd2eb12e870
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-30 10:17:02 -0500

    Completed HackRank exercise relating to sorting in Java.

---
+++ src/JavaSort.java
@@ -0,0 +1,60 @@
+/**
+ * HackerRank exercise that teaches the purpose and use of
+ * sorting and comparators in Java
+ *
+ * @author  Michael Jones
+ * @version 1.0
+ * @since   2022-5-29
+ */
+
+
+import java.util.*;
+import java.util.stream.*;
+
+class Student{
+    private int id;
+    private String fname;
+    private double cgpa;
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class JavaSort {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while(testCases>0){
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+
+        //--| Complete code here |--//
+        List<Student> orderedStudentList = studentList
+                .stream()
+                .sorted(Comparator.comparingDouble(Student::getCgpa).reversed().thenComparing(Student::getFname).thenComparing(Student::getId))
+                .collect(Collectors.toList());
+
+        orderedStudentList.stream().map(e -> e.getFname()).forEach(System.out::println);
+    }
+}
