dfd378680370e73908dee1e7aa4d7a84f89a3c64
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-02 20:46:25 -0500

    Codify Practice Question.

--- src/Workspace.java
+++ src/Workspace.java
@@ -5,16 +5,51 @@ import java.util.stream.*;
 
 public class Workspace {
 
+    public int solution(int[] A) {
+        if (A.length < 2) return A.length;
+
+        int count = 2;
+        int highest = 2;
+        int currentA = A[0];
+        int currentB = A[1];
+
+        for(int i = 2; i < A.length; i++) {
+            if (i % 2 == 0){
+                if (A[i] == currentA) {
+                    count++;
+                    if (count > highest) highest = count;
+                }
+                else count = 2;
+                currentA = A[i];
+            } else {
+                if (A[i] == currentB) {
+                    count++;
+                    if (count > highest) highest = count;
+                }
+                else count = 2;
+                currentB = A[i];
+            }
+        }
+
+        return highest;
+    }
+
     public static void main (String [] args) {
 
-        List <String> names = new ArrayList<String>(Arrays.asList("Ava", "Aneri", "ALberto"));
+        Workspace workspace = new Workspace();
+//        List <String> names = new ArrayList<String>(Arrays.asList("Ava", "Aneri", "ALberto"));
+//
+//        List <String> sorted = names
+//                .stream()
+//                .sorted()
+//                .collect(Collectors.toList());
+//
+//        System.out.println(sorted);
 
-        List <String> sorted = names
-                .stream()
-                .sorted()
-                .collect(Collectors.toList());
+        int [] a = new int[]{7, 4, -2, 4, -2, -9};
 
-        System.out.println(sorted);
+        System.out.println("solution() = " + workspace.solution(a));
+
     }
 
 }
