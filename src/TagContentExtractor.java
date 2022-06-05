27ec7b935387c09a39d83b9177ad0a4ea76232ff
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-04 20:26:23 -0500

    Completed HackerRank exercise Tag Content Extractor.

--- src/TagContentExtractor.java
+++ src/TagContentExtractor.java
@@ -1,13 +1,18 @@
-import java.io.*;
+/**
+ * HackerRank exercise that teaches how parsing works within an editor or IDE.
+ *
+ * @author  Michael Jones
+ * @version 1.0
+ * @since   2022-06-04
+ */
+
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 
 public class TagContentExtractor {
 
     public static char type(String line) {
+        if(line.length() < 2) return 'e';
         if(line.charAt(0) == '<') {
             if(line.charAt(1) == '/') return 'c';
             else return 'o';
@@ -15,36 +20,47 @@ public class TagContentExtractor {
     }
 
     public static void parse(String line) {
+        if(line.length() < 8) {
+            System.out.println("None");
+            return;
+        }
 
         boolean flag = false;
         String openingTag = "";
+        String string = "";
         String closingTag = "";
-        String content = "";
 
         loop: while (line.length() > 0) {
             switch (type(line)) {
                 case 'o': {
-                    if(line.indexOf(">") < 0) break loop;
+                    string = "";
+                    if(!line.contains(">")) break loop;
+
                     openingTag = line.substring(1, line.indexOf(">"));
                     line = line.substring(line.indexOf(">") + 1);
                 } break;
-                case 'c': {
-                    if(line.indexOf(">") < 0) break loop;
-                    closingTag = line.substring(2, line.indexOf(">"));
-                    if (openingTag.equals(closingTag) && !openingTag.equals("") && !content.equals("")) {
-                        flag = true;
-                        System.out.println(content);
+                case 's': {
+                    if(!line.contains("<")) break loop;
 
+                    string = line.substring(0, line.indexOf("<"));
+                    line = line.substring(line.indexOf("<"));
+
+                    if(openingTag.equals("")) string = "";
+                } break;
+                case 'c': {
+                    if(!line.contains(">")) break loop;
+
+                    closingTag = line.substring(2, line.indexOf(">"));
+                    line = line.substring(line.indexOf(">") + 1);
+
+                    if (openingTag.equals(closingTag) && !openingTag.equals("") && !string.equals("")) {
+                        flag = true;
+                        System.out.println(string);
                     }
                     openingTag = "";
-                    content = "";
-                    line = line.substring(line.indexOf(">") + 1);
+                    string = "";
                 } break;
-                case 's': {
-                    if(line.indexOf("<") < 0) break loop;
-                    content = line.substring(0, line.indexOf("<"));
-                    line = line.substring(line.indexOf("<"));
-                } break;
+                default: break loop;
             }
         }
         if(!flag) System.out.println("None");
@@ -52,7 +68,13 @@ public class TagContentExtractor {
 
     public static void main(String[] args){
 
-        parse("<>hello</><h>dim</h>>>>>");
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+            parse(line);
+            testCases--;
+        }
 
     }
 }
