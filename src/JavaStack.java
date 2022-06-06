9d3417456a3225f607f4609f1b360e2125bb6828
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-05 23:29:28 -0500

    Working on HackerRank exercise relating to Java stacks.

--- src/JavaStack.java
+++ src/JavaStack.java
@@ -2,14 +2,31 @@ import java.util.*;
 
 public class JavaStack {
 
-    public static void main(String []argh)
-    {
-        Scanner get = new Scanner(System.in);
 
-        while (get.hasNext()) {
-            String input = get.next();
-            //Complete the code
+    public static void process(String input) {
+
+        List<String> symbols = Arrays.asList(input.split(""));
+        Stack<String> stack = new Stack<>();
+
+        HashMap<String, String> symbolPairs = new HashMap<String, String>();
+        symbolPairs.put("{","}");
+        symbolPairs.put("}","{");
+        symbolPairs.put("(",")");
+        symbolPairs.put(")","(");
+        symbolPairs.put("[","]");
+        symbolPairs.put("]","[");
+
+        for(String symbol: symbols) {
+            if (!stack.empty() && stack.peek().equals(symbolPairs.get(symbol))) stack.pop();
+            else stack.push(symbol);
         }
+        System.out.println(stack.empty());
+    }
+
+
+    public static void main(String [] argh) {
+
+        process("{}(");
 
     }
 
