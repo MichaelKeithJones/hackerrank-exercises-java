78bc36eb6f7f158080dc9f4d0be9fe76733bb12a
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-06 20:44:40 -0500

    Created class for HackerRank exercise Java-256.

---
+++ src/JavaSHA256.java
@@ -0,0 +1,11 @@
+/**
+ * HackerRank exercise that teaches the purpose and use of
+ * a one dimensional array.
+ *
+ * @author  Michael Jones
+ * @version 1.0
+ * @since   2022-6-6
+ */
+
+public class JavaSHA256 {
+}
