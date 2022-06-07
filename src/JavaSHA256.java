2c70aff9466e2bbce7b18f6c08c5186b1ae832b4
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-06 21:04:13 -0500

    Completed HackerRank exercise on SHA-256.

--- src/JavaSHA256.java
+++ src/JavaSHA256.java
@@ -1,6 +1,11 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
 /**
  * HackerRank exercise that teaches the purpose and use of
- * a one dimensional array.
+ * SHA-256 cryptography
  *
  * @author  Michael Jones
  * @version 1.0
@@ -8,4 +13,22 @@
  */
 
 public class JavaSHA256 {
+
+    public static void main(String[] args)  {
+
+        try {
+            Scanner get = new Scanner(System.in);
+            String input = get.nextLine();
+
+            byte[] hash = MessageDigest.getInstance("SHA-256").digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte i : hash) hexString.append(String.format("%02x", i));
+
+            System.out.println(hexString.toString());
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Didn't work");
+        }
+
+    }
+
 }
