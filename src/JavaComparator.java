bd5714f73ba7a1b0ea525cdd3e7131a6815dfd0c
Michael Jones <michael.k.jones.20@gmail.com>
2022-06-06 22:43:04 -0500

    Completed HackerRank exercise for Comparators.

---
+++ src/JavaComparator.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Player{
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName () {return name;}
+    public int getScore () {return score;}
+}
+
+
+public class JavaComparator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.stream(player).sorted(Comparator.comparingInt(Player::getScore).reversed().thenComparing(Player::getName)).forEach(e -> System.out.println(e.name + " " + e.score));
+
+    }
+}
